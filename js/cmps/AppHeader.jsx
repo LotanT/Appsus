7feabb9f64fcdd0aa6777d69485bd63024940068
Ofek Ben Baruch <Ofekbb@gmail.com>
2022-01-01 18:43:42 +0200

    css fixes

--- js/cmps/AppHeader.jsx
+++ js/cmps/AppHeader.jsx
@@ -44,7 +44,7 @@ class _AppHeader extends React.Component {
 
         <header className="app-header">
           <h1 onClick={() => this.props.history.push('/')}>
-            <img src="../imgs/favicon.png" />
+            <img src="imgs/favicon.png" />
             <span>AppSus</span>
           </h1>
 
@@ -60,7 +60,7 @@ class _AppHeader extends React.Component {
           </div>}
 
 
-          <img onClick={this.toggleModal} src="../imgs/apps-grid.png" />
+          <img onClick={this.toggleModal} src="imgs/apps-grid.png" />
         </header>
 
 
@@ -68,11 +68,11 @@ class _AppHeader extends React.Component {
         <div onClick={this.closeModal} className={`screen-modal ${activeModal}`}></div>
         <nav className={`main-nav ${activeModal}`}>
           <NavLink onClick={this.closeModal} activeClassName="my-active" exact to="/">
-            <img src="../imgs/app/nav/homepage.png" /><span>Home</span>
+            <img src="imgs/app/nav/homepage.png" /><span>Home</span>
           </NavLink>
           {/* <NavLink to="/about">About</NavLink> */}
-          <NavLink onClick={this.closeModal} to="/notes"><img src="../imgs/app/nav/google-keep.png" /><span>Notes</span></NavLink>
-          <NavLink onClick={this.closeModal} to="/mail"><img src="../imgs/app/nav/gmail.png" /><span>Mail</span></NavLink>
+          <NavLink onClick={this.closeModal} to="/notes"><img src="imgs/app/nav/google-keep.png" /><span>Notes</span></NavLink>
+          <NavLink onClick={this.closeModal} to="/mail"><img src="imgs/app/nav/gmail.png" /><span>Mail</span></NavLink>
           <NavLink onClick={this.closeModal} to="/book">Our Books</NavLink>
         </nav>
 
